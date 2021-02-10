8a0c929a84aeb3a92658d846813c59d3a3f1926d
rebahozkoc <rebahozkoc@hotmail.com>
2021-02-10 12:53:15 +0300

    Added a new button to create a new note.

--- app/src/main/java/com/example/todoapp/ui/create/CreateActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/create/CreateActivity.kt
@@ -1,7 +1,10 @@
 package com.example.todoapp.ui.create
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat.startActivity
 import com.example.todoapp.R
 
 class CreateActivity : AppCompatActivity() {
@@ -9,4 +12,15 @@ class CreateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
     }
+
+
+
+    companion object {
+        fun open(context: Context){
+            val intent = Intent(context, CreateActivity::class.java)
+            context.startActivity(intent)
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/todoapp/ui/list/ListActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/list/ListActivity.kt
@@ -1,12 +1,17 @@
 package com.example.todoapp.ui.list
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.example.todoapp.R
+import com.example.todoapp.ui.create.CreateActivity
 
 class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        val addNoteButton = findViewById<FloatingActionButton>(R.id.addNoteButton)
+        addNoteButton.setOnClickListener{CreateActivity.open(this)}
     }
 }
