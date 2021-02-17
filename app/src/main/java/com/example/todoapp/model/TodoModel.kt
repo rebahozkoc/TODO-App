d794dffec37a313057d979195c27d2121f5b53e1
Arda Altunyay <ardaltunyay@gmail.com>
2021-02-17 19:00:03 +0300

    Update TodoModel

--- app/src/main/java/com/example/todoapp/model/TodoModel.kt
+++ app/src/main/java/com/example/todoapp/model/TodoModel.kt
@@ -1,21 +1,13 @@
+package com.example.todoapp.model
+
 import android.os.Parcelable
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import com.example.todoapp.model.TodoState
 import kotlinx.parcelize.Parcelize
 
-@Entity(tableName = "task_data_table")
 @Parcelize
 data class TodoModel(
-    @ColumnInfo(name = "task_data_title")
     val title: String,
-    @ColumnInfo(name = "task_data_description")
     val description: String,
-    @ColumnInfo(name = "task_data_date")
     val createdDate: String,
-    @ColumnInfo(name = "task_data_status")
     val status: TodoState,
-    @PrimaryKey(autoGenerate = true)
-    var id: Long = 0L
-): Parcelable
+    var id: Long = 0
+) : Parcelable
