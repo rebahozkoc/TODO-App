f250a42bbd8898bd86a4ac47541835efa95163d5
Arda Altunyay <ardaltunyay@gmail.com>
2021-02-17 18:59:53 +0300

    Create TodoMapper

---
+++ app/src/main/java/com/example/todoapp/data/mapper/TodoMapper.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp.data.mapper
+
+import com.example.todoapp.data.entity.TodoEntity
+import com.example.todoapp.model.TodoModel
+
+object TodoMapper {
+
+    fun toTodoEntity(todoModel: TodoModel): TodoEntity {
+        return TodoEntity(
+            id = todoModel.id,
+            title = todoModel.title,
+            description = todoModel.description,
+            createdDate = todoModel.createdDate,
+            status = todoModel.status
+        )
+    }
+
+    fun toTodoModel(todoEntity: TodoEntity): TodoModel {
+        return TodoModel(
+            title = todoEntity.title,
+            description = todoEntity.description,
+            createdDate = todoEntity.createdDate,
+            status = todoEntity.status
+        )
+    }
+}
